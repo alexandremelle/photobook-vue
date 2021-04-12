c3198b51eac018aa51b4c11504ac1be071dba05b
Alexandre Melle <alex.melle90@gmail.com>
2021-04-12 16:35:30 +0200

    Add createPhoto action

--- src/store/albums/albums.js
+++ src/store/albums/albums.js
@@ -1,7 +1,10 @@
 import { createAlbum as createAlbumMutation } from "@/graphql/mutations";
 import { getAlbum as getAlbumQuery } from "@/graphql/queries";
 import { listAlbums as listAlbumsQuery } from "@/graphql/queries";
-import API, { graphqlOperation } from "@aws-amplify/api";
+import { createPhoto as createPhotoMutation } from "@/graphql/mutations";
+import API, { graphqlOperation, Storage } from "@aws-amplify/api";
+import { uuid } from "uuidv4";
+import awsconfig from "@/aws-exports";
 
 export const albumInfo = {
   namespaced: true,
@@ -30,6 +33,41 @@ export const albumInfo = {
     async getAlbumsData({ commit }) {
       const albumData = await API.graphql(graphqlOperation(listAlbumsQuery));
       commit("setAlbums", albumData.data.listAlbums.items);
+    },
+    async createPhoto(_, data) {
+      const {
+        aws_user_files_s3_bucket_region: region,
+        aws_user_files_s3_bucket: bucket,
+      } = awsconfig;
+      const { file, type: mimetype, id } = data;
+      const extension = file.name.substr(file.name.lastIndexOf("." + 1));
+      const photoId = uuid();
+      const key = `images/${photoId}.${extension}`;
+      const inputData = {
+        id: photoId,
+        photoAlbumId: id,
+        contentType: mimetype,
+        fullsize: {
+          key,
+          region,
+          bucket,
+        },
+      };
+      // S3 bucket storage add file to it
+      try {
+        await Storage.put(key, file, {
+          level: "protected",
+          contentType: mimetype,
+          metadata: { albumId: id, photoId },
+        });
+        await API.graphql(
+          graphqlOperation(createPhotoMutation, { input: inputData })
+        );
+        return Promise.resolve("success");
+      } catch (error) {
+        console.error("createPhoto error", error);
+        return Promise.reject(error);
+      }
     },
   },
   getters: {
