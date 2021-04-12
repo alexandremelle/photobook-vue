721f777c63e27cabc381134b672c3e93d12e1d75
Alexandre Melle <alex.melle90@gmail.com>
2021-04-12 16:02:27 +0200

    Add SignUpPage AlbumDetailPage and AlbumPage

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import SignUpPage from "../views/SignUpPage.vue";
+import AlbumDetailPage from "../views/AlbumDetailPage.vue";
+import AlbumsPage from "../views/AlbumsPage.vue";
+import Auth from "@aws-amplify/auth";
 
 Vue.use(VueRouter);
 
@@ -9,6 +13,23 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/signup",
+    name: "SignUpPage",
+    component: SignUpPage,
+  },
+  {
+    path: "/album/:id",
+    name: "AlbumDetailPage",
+    component: AlbumDetailPage,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/albums",
+    name: "AlbumsPage",
+    component: AlbumsPage,
+    meta: { requiresAuth: true },
   },
   {
     path: "/about",
@@ -25,6 +46,16 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  const isAuthenticated = await Auth.currentUserInfo();
+  if (requiresAuth && !isAuthenticated) {
+    next("/");
+  } else {
+    next();
+  }
 });
 
 export default router;
